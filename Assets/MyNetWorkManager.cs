fed8207f191c418b3691033c28dbbfcfacc09b24
Escafermo <31081737+Escafermo@users.noreply.github.com>
2018-01-09 21:21:43 -0200

    Piratez 0.1

---
+++ Assets/MyNetWorkManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyNetWorkManager : NetworkManager {
+
+    public void MyStartHost()
+    {
+        StartHost();
+        Debug.Log("Starting host at " + Time.timeSinceLevelLoad);
+    }
+
+    public override void OnStartHost()
+    {
+        Debug.Log("Host started at " + Time.timeSinceLevelLoad);
+    }
+
+}
