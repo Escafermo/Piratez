a735268989f02a7b571b79f008601877148ae629
Escafermo <31081737+Escafermo@users.noreply.github.com>
2018-01-10 10:21:54 -0200

    NetWork Client-side

--- Assets/MyNetWorkManager.cs
+++ Assets/MyNetWorkManager.cs
@@ -16,4 +16,13 @@ public class MyNetWorkManager : NetworkManager {
         Debug.Log("Host started at " + Time.timeSinceLevelLoad);
     }
 
+    public override void OnStartClient(NetworkClient myClient)
+    {
+        Debug.Log("Client start requested at " + Time.timeSinceLevelLoad);
+    }
+
+    public override void OnClientConnect(NetworkConnection myConnection)
+    {
+        Debug.Log("Client is connected at " + Time.timeSinceLevelLoad + " - On IP: " + myConnection.address);
+    }
 }
